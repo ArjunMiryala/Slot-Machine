c998dae7d2dd83fa544d7d48bc1dec2d9e081922
Arjun Bharadwaj <m.arjunbharadwaj@gmail.com>
2025-02-28 15:56:53 -0700

    afer deposit method

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-dwasdwa
+def deposit():                                  # defining a function for user deposit
+    while True:                                 # looping until we get valid amount
+        amount = input("How much would you lke to deposit ?₹")
+        if amount.isdigit():                   # cheking if it is a number (0 to inf)
+            amount = int(amount)               # convert to int              
+            if amount > 0:                            # amount >than 0
+                break                               # break the loop if it is
+            else:
+                print("Please deposit ruppes greater than zero")
+        else:
+            print("please deposit valid amount")
+    return amount                              #return the amount
+
+balance = deposit()
